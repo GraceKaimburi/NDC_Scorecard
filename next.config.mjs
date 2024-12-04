6ec4bf6198a65a2202ab364fc4af7eefa5c2458c
GraceKaimburi <gracekaimburi123@gmail.com>
2024-12-04 10:20:12 +0300

    Update next.config.mjs trying to fix isues

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
     images: {
       domains: ['ndcbackend.agnesafrica.org'],
     },
-    experimental: {
-      serverActions: {
-        enabled: true
-      }
-    }
+    // experimental: {
+    //   serverActions: {
+    //     enabled: true
+    //   }
+    // }
   };
   
   export default nextConfig;
