05aa100cb8c7a3f84dcc901c27ddd843e9fa2c85
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-12-17 02:44:25 +0300

    Modified layout for different capacity categories

--- app/(user)/dashboard/layout.jsx
+++ app/(user)/dashboard/layout.jsx
@@ -4,67 +4,65 @@ import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { DashboardProvider } from "@/store/DashboardContext";
 import { getRefreshToken, removeTokens } from "@/utils/access-token";
+import MaxWidth from "@/components/max-width";
 
 function Layout({ children }) {
-  const router = useRouter();
+	const router = useRouter();
 
-  const refreshToken = getRefreshToken();
+	const refreshToken = getRefreshToken();
 
-  const [timeLeftToExpire, setTimeLeftToExpire] = useState(0);
-  const [isRefreshTokenValid, setIsRefreshTokenValid] = useState(false);
-  const [showSessionWarning, setShowSessionWarning] = useState(false);
-  const warningThreshold = 5 * 60; // 5 minutes in seconds
-  const [timerInterval] = useState(1000);
+	const [timeLeftToExpire, setTimeLeftToExpire] = useState(0);
+	const [isRefreshTokenValid, setIsRefreshTokenValid] = useState(false);
+	const [showSessionWarning, setShowSessionWarning] = useState(false);
+	const warningThreshold = 5 * 60; // 5 minutes in seconds
+	const [timerInterval] = useState(1000);
 
-  const refreshExpiry = useMemo(() => {
-    if (refreshToken) {
-      const decoded = jwtDecode(refreshToken);
-      return decoded.exp;
-    }
-    return null;
-  }, [refreshToken]);
+	const refreshExpiry = useMemo(() => {
+		if (refreshToken) {
+			const decoded = jwtDecode(refreshToken);
+			return decoded.exp;
+		}
+		return null;
+	}, [refreshToken]);
 
-  useEffect(() => {
-    if (refreshExpiry) {
-      const now = Math.floor(Date.now() / 1000);
-      if (refreshExpiry > now) {
-        setIsRefreshTokenValid(true);
-        const timeLeft = refreshExpiry - now;
-        setTimeLeftToExpire(timeLeft);
-      } else {
-        setIsRefreshTokenValid(false);
-        removeTokens();
-        router.push("/login");
-      }
-    }
-  }, [refreshExpiry, router]);
+	useEffect(() => {
+		if (refreshExpiry) {
+			const now = Math.floor(Date.now() / 1000);
+			if (refreshExpiry > now) {
+				setIsRefreshTokenValid(true);
+				const timeLeft = refreshExpiry - now;
+				setTimeLeftToExpire(timeLeft);
+			} else {
+				setIsRefreshTokenValid(false);
+				removeTokens();
+				router.push("/login");
+			}
+		}
+	}, [refreshExpiry, router]);
 
-  useEffect(() => {
-    if (timeLeftToExpire > 0) {
-      const timerId = setInterval(() => {
-        setTimeLeftToExpire((prev) => prev - 1);
-      }, timerInterval);
+	useEffect(() => {
+		if (timeLeftToExpire > 0) {
+			const timerId = setInterval(() => {
+				setTimeLeftToExpire((prev) => prev - 1);
+			}, timerInterval);
 
-      if (timeLeftToExpire <= warningThreshold && !showSessionWarning) {
-        setShowSessionWarning(true); // Show the warning when time left is below threshold
-      }
+			if (timeLeftToExpire <= warningThreshold && !showSessionWarning) {
+				setShowSessionWarning(true); // Show the warning when time left is below threshold
+			}
 
-      return () => clearInterval(timerId);
-    }
-  }, [timeLeftToExpire, timerInterval, showSessionWarning, warningThreshold]);
+			return () => clearInterval(timerId);
+		}
+	}, [timeLeftToExpire, timerInterval, showSessionWarning, warningThreshold]);
 
-  if (!isRefreshTokenValid) {
-    return null; // Prevent rendering if the refresh token is invalid
-  }
+	if (!isRefreshTokenValid) {
+		return null; // Prevent rendering if the refresh token is invalid
+	}
 
-  return (
-
-    <DashboardProvider>
-
-        {children}
-
-    </DashboardProvider>
-  );
+	return (
+		<DashboardProvider>
+			<MaxWidth className={'w-full min-h-[60vh]'}>{children}</MaxWidth>
+		</DashboardProvider>
+	);
 }
 
 export default Layout;
